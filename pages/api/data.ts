17e346f2ba0522ec8d6eb352bcf6f7b515e6062a
Gebec <gebec.m@gmail.com>
2022-11-12 23:53:46 +0100

    chore: added prettier

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -2,19 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import data from '../../src/__nemazat/data.json'
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-
-
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const getRandomInt = (max: number) => {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max)
   }
 
   const newData = {
     count: 30,
-    items: data.items.map(({ location, instantBookable, name, passengersCapacity, pictures, sleepCapacity, price, toilet, shower, vehicleType  }) => {
+    items: data.items.map(({ location, instantBookable, name, passengersCapacity, pictures, sleepCapacity, price, toilet, shower, vehicleType }) => {
       return {
         location,
         instantBookable,
@@ -28,7 +23,7 @@ export default function handler(
         // toilet: [true, false][getRandomInt(2)],
         // shower: [true, false][getRandomInt(2)],
         // vehicleType: ['Campervan', 'Intergrated', 'Alcove', 'BuiltIn'][getRandomInt(4)],
-        pictures,
+        pictures
       }
     })
   }
